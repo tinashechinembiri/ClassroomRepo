c5af71601467cfcf77a6856c666a9bbe83504f6d
tinashechinembiri <tinashechinembiri5@gmail.com>
2018-11-16 11:07:44 +0000

    add the persistance data

---
+++ src/main/java/com/qa/classroom/persistance/Trainee.java
@@ -0,0 +1,46 @@
+package com.qa.classroom.persistance;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity 
+
+public class Trainee {
+	@Id
+	private long Traineeid; 
+	
+	@Column(length = 100)
+	private String Trainee_name;
+
+	public long getTraineeid() {
+		return Traineeid;
+	}
+
+	public void setTraineeid(long traineeid) {
+		Traineeid = traineeid;
+	}
+
+	public String getTrainee_name() {
+		return Trainee_name;
+	}
+
+	public void setTrainee_name(String trainee_name) {
+		Trainee_name = trainee_name;
+	}
+
+	public Trainee() {
+		super();
+		
+	}
+
+	public Trainee(long traineeid, String trainee_name) {
+		super();
+		Traineeid = traineeid;
+		Trainee_name = trainee_name;
+	} 
+	
+	
+	
+
+}
